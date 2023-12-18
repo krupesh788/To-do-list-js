d7ce8c29a82e62bee1dcfb03dabc09b63daf9808
krupesh788 <71176180+krupesh788@users.noreply.github.com>
2023-12-18 14:27:03 +0530

    Update script.js
    added some data

---
+++ script.js
@@ -0,0 +1,28 @@
+function addTask() {
+  var taskInput = document.getElementById("taskInput");
+  var taskList = document.getElementById("taskList");
+
+  if (taskInput.value.trim() !== "") {
+    var newTask = document.createElement("li");
+    newTask.innerHTML = `
+      ${taskInput.value}
+      <button onclick="editTask(this)">Edit</button>
+      <button onclick="deleteTask(this)">Delete</button>
+    `;
+    taskList.appendChild(newTask);
+    taskInput.value = ""; // Clear input after adding task
+  }
+}
+
+function editTask(button) {
+  var taskText = button.parentNode.firstChild;
+  var updatedTask = prompt("Edit task:", taskText.nodeValue);
+  if (updatedTask !== null) {
+    taskText.nodeValue = updatedTask;
+  }
+}
+
+function deleteTask(button) {
+  var taskItem = button.parentNode;
+  taskItem.parentNode.removeChild(taskItem);
+}
